0c25b123d10f5fbfbf670eda1e0150075015fed1
ZhangXichu <xichu.zhang@mensa.cz>
2024-01-27 16:31:41 +0100

    some experiments

--- my_array/main.cpp
+++ my_array/main.cpp
@@ -21,6 +21,24 @@ int main()
     std::cout << "current capacity : " << arr.get_capacity() << std::endl;
 
     std::cout << "the 7th element is :" << arr[6] << std::endl;
+    
+    int* ptr_x;
+    {
+        MyArray<int> arr_x(20);
+        std::cout << "Elements in arr_x :" << std::endl;
+        arr_x.print();
+        arr_x[10] = 100;
+        ptr_x = &arr_x[10];
+    }
+    std::cout << ptr_x << std::endl; // after MyArray arr_x is destoried, the address of the element is still kept
+    std::cout << *ptr_x << std::endl;
+
+    MyArray<int> arr_y(20);
+    arr_y[10] = 123;
+    int* ptr_y = &arr_y[10];
+    arr_y.resize(15);
+
+    std::cout << *ptr_y << std::endl; // what does ptr_y point to now?
 
     return 0;
 }
