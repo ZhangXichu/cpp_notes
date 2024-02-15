693013415b8df9c7f0703163bdd751a8ad5b7106
ZhangXichu <xichu.zhang@yahoo.com>
2024-02-15 12:25:47 +0100

    sequence inspired by Lisp

--- lisp_sequence/seq.hpp
+++ lisp_sequence/seq.hpp
@@ -1,15 +1,39 @@
-template<class T> class Seq {
+template<class T> class Sequence {
 public:
     // create and return a sequence with no elements
-    Seq();
+    Sequence();
     // create and return a sequence with first element t and subsequent elements
     // those of s
-    Seq(const T& t, const Seq& s); 
-    // return the first element of the Seq, Seq must have >= 1 elements
+    Sequence(const T& t, const Sequence& s); 
+    // return the first element of the Sequence, Sequence must have >= 1 elements
     T hd() const;
-    // create and return a sequence containing all but the first element of Seq, 
-    // Seq must have >= 1 elements
-    Seq tl() const;
-    // return false if Seq has no elements; true otherwise
+    // create and return a sequence containing all but the first element of Sequence, 
+    // Sequence must have >= 1 elements
+    Sequence tl() const;
+    // return false if Sequence has no elements; true otherwise
     operator bool() const;
 };
+
+
+template <class T> 
+class SequenceItem {
+    friend class Sequence<T>;
+
+    int use_count;
+    const T data;
+    SequenceItem* next;
+
+    SequenceItem(const T& t, SequenceItem* s);
+    SequenceItem(const T& t) : use_count(1), data(t), next(nullptr) {}
+};
+
+
+template <class T> 
+SequenceItem<T>::SequenceItem(const T& t, SequenceItem* s)
+    : use_count(1), data(t), next(s)
+{
+    if (s)
+    {
+        s->use_count++;
+    }
+}
