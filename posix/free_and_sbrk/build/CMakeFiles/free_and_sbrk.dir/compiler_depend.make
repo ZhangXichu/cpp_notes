6fd6caa46687b45f083129f749691e23d2da9ea3
ZhangXichu <xichu.zhang@yahoo.com>
2024-07-15 21:27:49 +0200

    posix exercises init

---
+++ posix/free_and_sbrk/build/CMakeFiles/free_and_sbrk.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for free_and_sbrk.
+# This may be replaced when dependencies are built.
---
+++ posix/free_and_sbrk/free_and_sbrk.cpp
@@ -0,0 +1,16 @@
+#include "tlpi_hdr.hpp"
+
+
+int main()
+{
+    char *ptr[MAX_ALLOCS];
+
+    int free_step = 1;
+    int free_min = 1;
+    int free_max = 1;
+    int num_allocs = 1000;
+    int block_size = num_allocs;
+    int j;
+
+    exit(EXIT_SUCCESS);
+}
