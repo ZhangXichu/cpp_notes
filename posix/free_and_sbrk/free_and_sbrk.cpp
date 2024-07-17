a4fd59275d01bcfe36bfed87e7ad1c1642d23004
ZhangXichu <xichu.zhang@yahoo.com>
2024-07-17 18:47:37 +0200

    program break before and after free

--- posix/free_and_sbrk/free_and_sbrk.cpp
+++ posix/free_and_sbrk/free_and_sbrk.cpp
@@ -7,10 +7,34 @@ int main()
 
     int free_step = 1;
     int free_min = 1;
-    int free_max = 1;
-    int num_allocs = 1000;
-    int block_size = num_allocs;
-    int j;
+    int free_max = 100;
+    int num_allocs = 100;
+    int block_size = 4;
+    int i;
+
+    printf("Initial program break: %10p\n", sbrk(0));
+
+    printf("Allocating %d*%d bytes\n", num_allocs, block_size);
+
+    for (int i = 0; i < num_allocs; i++)
+    {
+        ptr[i] = (char*)malloc(block_size);
+        if (ptr[i] == NULL)
+        {
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    printf("Program break is now: %10p\n", sbrk(0));
+
+    printf("Freeing blocks from %d to %d in steps of %d\n", free_min, free_max, free_step);
+
+    for (int i = free_min - 1; i < free_max; i += free_step)
+    {
+        free(ptr[i]);
+    }
+
+    printf("after free(), program break is %10p\n", sbrk(0));
 
     exit(EXIT_SUCCESS);
 }
